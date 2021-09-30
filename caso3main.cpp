c8dee921c48e289c8ac14dbc0234ab65912e0274
Elmono56 <78763533+Elmono56@users.noreply.github.com>
2021-09-30 14:36:08 -0600

    paletas

--- caso3main.cpp
+++ caso3main.cpp
@@ -5,11 +5,75 @@
 #include "lib/nodo.h"
 #include "lib/listadoble.h"
 #include "lib/stack.h"
+#include "lib/paletas.h"
 
 using namespace std;
 
 int main() {
 
+
+    struct paleta arroz1 = {25};
+    struct paleta arroz2 = {25};
+    struct paleta arroz3 = {25};
+    struct paleta arroz4 = {25};
+    struct paleta arroz5 = {25};
+    struct paleta arroz6 = {25};
+
+    struct nodo a1;
+    a1.data = &arroz1;
+
+    struct nodo a2;
+    a2.data = &arroz2;
+
+    struct nodo a3;
+    a3.data = &arroz3;
+
+    struct nodo a4;
+    a4.data = &arroz4;
+    
+    struct nodo a5;
+    a5.data = &arroz5;
+
+    struct nodo a6;
+    a6.data = &arroz6;
+
+    struct listadoble columnaa1;
+    struct stack columnaarroz = {columnaa1};
+
+    columnaarroz.push(&a1);
+
+    columnaarroz.push(&a2);
+
+    columnaarroz.push(&a3);
+
+    columnaarroz.push(&a4);
+
+    columnaarroz.push(&a5);
+
+    columnaarroz.push(&a6);
+
+    struct bodega bodegaarroz={columnaarroz};
+
+        
+    bodegaarroz.retirarProducto(75);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
     //bodega = {columnas,paletas,unidades}
     struct bodega arroz = {10,15,24}; 
     struct bodega frijoles = {8,20,15};
@@ -46,4 +110,5 @@ int main() {
     vacia = pila.isEmpty();
 
     cout<<vacia<<endl;
+    */
 }
